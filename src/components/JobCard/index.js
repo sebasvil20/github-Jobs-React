f6fc47519ea4f2eeb4bd5d7ccb6c4a6b81c2dddf
Sebastian Villegas <sebasvil20@gmail.com>
2021-04-17 13:21:16 -0500

    Styling job cards

--- src/components/JobCard/index.js
+++ src/components/JobCard/index.js
@@ -1,7 +1,28 @@
-import {CardContainer, Image, Company, Title, Badge, TextContainer} from './styles'
+import {
+  CardContainer,
+  Image,
+  Company,
+  Title,
+  Badge,
+  TextContainer,
+} from './styles'
 
-export const JobCard = () => {
-    return(
-        <h1>Job Card</h1>
-    )
+const DEFAULT_IMG = 'https://images.unsplash.com/photo-1585842630354-c6766ebf210d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
+
+export const JobCard = ({
+    img = DEFAULT_IMG,
+    title = 'Job Title',
+    company = 'Company name',
+    badge = 'Badge text',
+}) => {
+  return (
+    <CardContainer>
+      <Image src={img}/>
+      <TextContainer>
+        <Company>{company}</Company>
+        <Title>{title}</Title>
+        <Badge>{badge}</Badge>
+      </TextContainer>
+    </CardContainer>
+  )
 }
--- src/components/JobCard/styles.js
+++ src/components/JobCard/styles.js
@@ -1,25 +1,52 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const CardContainer = styled.div`
+  width: 70%;
+  height: 120px;
+  border-radius: 5px;
+  display: flex;
+  overflow: hidden;
+  align-items: center;
+  padding: 5px 15px;
+  margin-top: 30px;
+  background: white;
+  `
 
-`
-
-export const Image =  styled.img`
-
+export const Image = styled.img`
+  width: 110px;
+  height: 110px;
+  border-radius: 4px;
 `
 
 export const Company = styled.p`
-
+  font-size: 12px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  line-height: 14.06px;
 `
 
 export const Title = styled.h1`
-
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  line-height: 21.09px;
+  font-weight: 400;
 `
 
 export const Badge = styled.p`
+font-size: 12px;
+font-weight: 700;
+line-height: 14.06px;
+padding: 10px;
+border: 1px solid #334680;
+border-radius: 4px;
 
 `
 
 export const TextContainer = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+  height: 100%;
+  padding: 10px;
 `
